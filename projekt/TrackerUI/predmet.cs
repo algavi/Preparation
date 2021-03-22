bfc1baf5bd1f48782c14b2e9a687da80abe45c88
unknown <alesalgavibartos@gmail.com>
2021-03-22 14:58:56 +0100

    projekt

--- projekt/TrackerUI/predmet.cs
+++ projekt/TrackerUI/predmet.cs
@@ -10,12 +10,11 @@ using System.Windows.Forms;
 
 namespace TrackerUI
 {
-    public partial class Form1 : Form
+    public partial class predmet : Form
     {
-        public Form1()
+        public predmet()
         {
             InitializeComponent();
         }
-
     }
 }
